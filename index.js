fd80c3652b2bde823109499b535ad0a71123acfb
mahamadouf <123761069+mahamadouf@users.noreply.github.com>
2025-01-24 20:12:35 +0000

    fofana

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ const inventory = [
 ]
 
 // Task 1: Set the view engine to EJS. 
+app.set("view engine", "ejs")
 
 
 
@@ -73,24 +74,34 @@ app.use((req, res, next) => {
 })
 
 // Task 1: Set up the static middleware
+app.use(express.static(__dirname + "/public"))
 
 
 
 // Task 2: Set up the route handler for / to send back the index.html file
 
-
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/index.html")
+})
 
 // Task 3: Set up the route handler for /mens which sends back category.ejs with the men's category object
 
-
+app.get("/mens", (request, response) => {
+  response.render("category.ejs", inventory[0])
+})
 
 // Task 4: Plug in the values in category.ejs to get the page working
 // PINK ONLY: Set up a route handler for /womens to pass in similar data for women's
 
-
+// app.get("/womens", (request, response) => {
+//   response.render("category.ejs", inventory[1].category)
+// })
 
 // Task 5: Set up the route handler for /item/0 which sends back the first item in product.ejs
 
+// app.get("/item/0", (request, response) => {
+//   response.send()
+// })
 
 
 // Task 6: Plug in the values in product.ejs to get the page working
